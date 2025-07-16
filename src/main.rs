b494256095d257e9b48a07b590868764b2b06d3a
Tebay <95620167+KaiTebay@users.noreply.github.com>
2025-07-16 17:00:13 +0100

    Processing a guess

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+// To obtain user input and print outputs, use the input/output library from the standard  library.
+use std::io;
+
+// Define the main function with no parameters and the following body:
+fn main() {
+	// Use the println! macro to print strings to introduce the game
+	println!("Guess the number!");
+
+	println!("Please input your guess.");
+
+	// Define a mutable (the value can change) variable called guess
+	// Bind guess to 'new', an associated function of the String type from the standard library.
+	// i.e. 'guess' is a variable starting as an empty string.
+	let mut guess = String::new();
+	
+	// Call the stdin function from std:io	
+	io::stdin()
+		// Call the .read_line method to take the user's standard input and append it to a string.
+		// By passing a mutable reference ('&mut') to guess as the argument, tell .read_line which string to append to.
+		.read_line(&mut guess)
+		// In addition to the string value, the .read_line method will return an enum Result about its success.
+		// Here, we should handle errors. In this case, the .expect method will simply crash the program.
+		.expect("Failed to read line");
+
+	// Use the println! macro to print the user's guess.
+	// This println! uses curly brackets and the variable name 'guess' as a placeholder for the variable's value.
+	println!("You guessed: {guess}");
+}
